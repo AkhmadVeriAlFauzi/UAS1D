c03d970efd0372db987369d0c27bf9c8cceb0512
AkhmadVeriAlFauzi <alfauziveri@gmail.com>
2023-01-17 11:39:46 +0700

    VERRY

--- src/UAS1D.java
+++ src/UAS1D.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class UAS1D {
+import java.util.ArrayList;
+
+public class UAS1D{
+    private String MessageID;
+    private String MessageName;
+    private String Context;
+
+    public String getMessageID() {
+        return MessageID;
+    }
+
+    public void setMessageID(String messageID) {
+        MessageID = messageID;
+    }
+
+    public String getMessageName() {
+        return MessageName;
+    }
+
+    public void setMessageName(String messageName) {
+        MessageName = messageName;
+    }
+
+    public String getContext() {
+        return Context;
+    }
+
+    public void setContext(String context) {
+        Context = context;
+    }
+    public UAS1D(){
+        this.MessageID = MessageID;
+        this.MessageName = MessageName;
+        this.Context = Context;
+    }
+    public String FindWords(ArrayList<String> context, String cari){
+        String HURUF = String.valueOf(context.indexOf(cari));
+        return HURUF;
+    }
 }
+
